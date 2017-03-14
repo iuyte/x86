ead6fe97cbd5cf61f625063dcaddff585e0b3cff
iuyte <ethanialw@gmail.com>
2017-03-13 21:27:55 -0400

    UPDATE

--- plugins/misc.lua
+++ plugins/misc.lua
@@ -2,6 +2,10 @@ event.listen("command", "pong", function(txt, message)
 	return "ping"
 end)
 
+event.listen("command", "ping", function(txt, message)
+	return "pong"
+end)
+
 event.listen("command", "id", function(txt, message)
 	return message.member.id
 end)
